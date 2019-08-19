880d4e750f0edc54e61e1176b4b485e0cf2931e2
Philemon Berhane <philberhane@yahoo.com>
2019-08-18 19:02:00 -0700

    corrections

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = {
     
    sayHello(req, res) {
     var name = req.body.name;
-    res.status(200).send({message: "Hello " + name});
+    res.status(200).send({message: "Hello"});
 
 }
     
