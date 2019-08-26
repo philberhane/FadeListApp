2f9a2e8a8960aaf00f864f6651751bf139ac4d06
Philemon Berhane <philberhane@yahoo.com>
2019-08-26 00:09:11 -0700

    few changes to signup route

--- routes/index.js
+++ routes/index.js
@@ -181,8 +181,8 @@ module.exports = {
     },
 
     receiveText(req, res) {
-        console.log(req.body.Body); // the message body
-        console.log(req.body.From);
+        console.log(req.body); // the message body
+       // console.log(req.body.From);
         res.status(200).send({message: "Success"});
 
     }
