fcad5bd0f1d3ed6dd0e3706b4c1d81c825c0aedb
Philemon Berhane <philberhane@yahoo.com>
2019-08-26 18:49:48 -0700

    few changes to signup route

--- routes/index.js
+++ routes/index.js
@@ -208,7 +208,16 @@ module.exports = {
                  var phone = userSnapshot.val().phone.split(' ')
                  var phoneNumber = phone[0] + phone[1] + phone[2] + phone[3]
                 if (phoneNumber === req.body.To) {
-                 console.log(userSnapshot.val().waitlist)
+                    var ref = firebase.database().ref('/users/'+userSnapshot.val().username);
+                    var array = userSnapshot.val().waitlist
+                    var obj = {
+                        cut: req.body.Body,
+                        number: req.body.From
+                    }
+                    array.push(obj)
+                    ref.child('waitlist').set({
+                     array
+                    });
                 }
              }
         })
