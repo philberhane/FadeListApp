79f3550db63c2c3f326a152cfb3cf787de9da71d
Philemon Berhane <philberhane@yahoo.com>
2019-08-26 18:23:43 -0700

    few changes to signup route

--- routes/index.js
+++ routes/index.js
@@ -181,6 +181,24 @@ module.exports = {
     },
 
     receiveText(req, res) {
+
+        var firebase = require('firebase');
+        
+        var firebaseConfig = {
+            apiKey: "AIzaSyAaX_NmPwK2_K1E6Azmj5PFaOw5KhJsJfY",
+            authDomain: "nodebarbershopdatabase.firebaseapp.com",
+            databaseURL: "https://nodebarbershopdatabase.firebaseio.com",
+            projectId: "nodebarbershopdatabase",
+            storageBucket: "",
+            messagingSenderId: "393042645396",
+            appId: "1:393042645396:web:14b67934e1b60a69"
+          };
+          // Initialize Firebase
+          if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+
         console.log(req.body); // the message body
        console.log(req.body.From);
        console.log(req.body.Body);
